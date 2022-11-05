51b21977c63b83fc1f76fcd6cc23cbbda96368e8
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2022-11-05 17:46:20 -0300

    test cicd for develop with tests

--- src/auth/__test__/auth.service.spec.ts
+++ src/auth/__test__/auth.service.spec.ts
@@ -35,47 +35,47 @@ describe(AuthService.name, () => {
     });
   });
 
-  // describe('signUp', () => {
-  //   it('should throw if e-mail already exists', async () => {
-  //     const signUpUser = { email: 'new_user@gmail.com', password: '123' };
-  //     await userRepositoryMock.insert(signUpUser);
-  //     const response = sut.signUp(signUpUser);
-  //     expect(response).rejects.toThrow(BadRequestException);
-  //   });
+  describe('signUp', () => {
+    it('should throw if e-mail already exists', async () => {
+      const signUpUser = { email: 'new_user@gmail.com', password: '123' };
+      await userRepositoryMock.insert(signUpUser);
+      const response = sut.signUp(signUpUser);
+      expect(response).rejects.toThrow(BadRequestException);
+    });
 
-  //   it('should return user and token in success', async () => {
-  //     const newUser = { email: 'new_user@gmail.com', password: '123' };
-  //     const response = await sut.signUp(newUser);
-  //     expect(response.user.email).toEqual(newUser.email);
-  //     expect(response.token).toBeTruthy();
-  //   });
-  // });
+    it('should return user and token in success', async () => {
+      const newUser = { email: 'new_user@gmail.com', password: '123' };
+      const response = await sut.signUp(newUser);
+      expect(response.user.email).toEqual(newUser.email);
+      expect(response.token).toBeTruthy();
+    });
+  });
 
-  // describe('refreshToken', () => {
-  //   it('should throw if the parameter is not a bearer token', async () => {
-  //     const result = sut.refreshToken({ oldToken: 'wrong_token' });
-  //     expect(result).rejects.toThrow(NotFoundException);
-  //   });
+  describe('refreshToken', () => {
+    it('should throw if the parameter is not a bearer token', async () => {
+      const result = sut.refreshToken({ oldToken: 'wrong_token' });
+      expect(result).rejects.toThrow(NotFoundException);
+    });
 
-  //   it('should throw if the provided token does not refer to an user', async () => {
-  //     jest.spyOn(jwtService, 'decode').mockImplementationOnce(() => ({
-  //       sub: 'wrong_user_id',
-  //     }));
-  //     const result = sut.refreshToken({ oldToken: 'Bearer ey...' });
-  //     expect(result).rejects.toThrow(NotFoundException);
-  //   });
+    it('should throw if the provided token does not refer to an user', async () => {
+      jest.spyOn(jwtService, 'decode').mockImplementationOnce(() => ({
+        sub: 'wrong_user_id',
+      }));
+      const result = sut.refreshToken({ oldToken: 'Bearer ey...' });
+      expect(result).rejects.toThrow(NotFoundException);
+    });
 
-  //   it('return a new token if everything is ok', async () => {
-  //     const signUpResult = await sut.signUp({
-  //       email: 'user@gmail.com',
-  //       password: '123123',
-  //     });
-  //     jest.spyOn(jwtService, 'decode').mockImplementationOnce(() => ({
-  //       sub: signUpResult.user._id,
-  //     }));
-  //     const result = await sut.refreshToken({ oldToken: 'Bearer ey...' });
-  //     expect(result.token).toBeTruthy();
-  //     expect(result.user).toBeTruthy();
-  //   });
-  // });
+    it('return a new token if everything is ok', async () => {
+      const signUpResult = await sut.signUp({
+        email: 'user@gmail.com',
+        password: '123123',
+      });
+      jest.spyOn(jwtService, 'decode').mockImplementationOnce(() => ({
+        sub: signUpResult.user._id,
+      }));
+      const result = await sut.refreshToken({ oldToken: 'Bearer ey...' });
+      expect(result.token).toBeTruthy();
+      expect(result.user).toBeTruthy();
+    });
+  });
 });
